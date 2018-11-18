2c3eb67ff8b631d1ee9a1b5499916fcb796fe977
Julian Marrades <julian.marrades@hotmail.es>
2018-11-18 16:02:09 +0100

    Good test

--- src/Test.java
+++ src/Test.java
@@ -9,9 +9,9 @@ public class Test {
         double[][] data = new double[][]{{1,1},{1,0},{0,1},{0,0}};
         double[][] targets = new double[][]{{0},{1},{1},{0}};
         double lr = 0.1;
-        double threshold = 0.01;
+        double threshold = 0.0005;
 
-        int[] layers = new int[]{2,2,2,1};
+        int[] layers = new int[]{2,3,3,1};
         NeuralNetwork net = new NeuralNetwork(layers);
 
         NNTrainer trainer = new Backpropagation(data, targets, threshold, lr);
